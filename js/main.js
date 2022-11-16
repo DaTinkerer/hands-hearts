93001f4e8ea9bfa2804084daf05551cc187adde6
bryant <bryantpeterson5@gmail.com>
2022-11-16 01:01:16 -0500

    nav works on mobile and desktop

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// navbar toggle
+const bars = document.querySelector(".fa-bars");
+const xmark = document.querySelector(".x-mark");
+const nav = document.querySelector(".nav");
+
+const showCloseMenu = () => {
+  nav.classList.toggle("active");
+};
+const closeMenu = () => {
+  if (screen.width >= 800) {
+    nav.classList.remove("active");
+  }
+};
+bars.addEventListener("click", showCloseMenu);
+xmark.addEventListener("click", showCloseMenu);
+addEventListener("resize", closeMenu);
